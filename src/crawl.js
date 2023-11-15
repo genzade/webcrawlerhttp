44e12187ff69cd8ecb9f4c7a6ed98c21aac3f774
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-11-15 23:52:20 +0000

    add crawl getURLsFromHTML + tests

--- src/crawl.js
+++ src/crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+	const urls = [];
+	const dom = new JSDOM(htmlBody);
+	const linkElements = dom.window.document.querySelectorAll("a");
+
+	for (const linkElement of linkElements) {
+		if (linkElement.href.startsWith("/")) {
+			try {
+				const urlObj = new URL(`${baseURL}${linkElement.href}`);
+				urls.push(urlObj.href);
+			} catch (error) {
+				console.log(`Error: ${error.message}`);
+			}
+		} else {
+			try {
+				const urlObj = new URL(linkElement.href);
+				urls.push(urlObj.href);
+			} catch (error) {
+				console.log(`Error: ${error.message}`);
+			}
+		}
+	}
+
+	return urls;
+}
+
 function normalizeURL(urlString) {
 	const urlObj = new URL(urlString);
 	const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -6,5 +34,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
+	getURLsFromHTML,
 	normalizeURL,
 };
--- test/crawl.test.js
+++ test/crawl.test.js
@@ -1,15 +1,17 @@
-const { normalizeURL } = require("../src/crawl");
-const { expect } = require("@jest/globals");
+const { getURLsFromHTML, normalizeURL } = require("../src/crawl");
+const { expect, describe } = require("@jest/globals");
 
 describe("normalizeURL", () => {
 	it("strips https protocol from the url input", () => {
 		const urlString = "https://example.com/path";
 		const actual = normalizeURL(urlString);
+
 		const expected = "example.com/path";
 
 		expect(actual).toEqual(expected);
 	});
 
+	// when("url input is a relative path", () => {
 	it("strips http protocol from the url input", () => {
 		const urlString = "http://example.com/path";
 		const actual = normalizeURL(urlString);
@@ -17,6 +19,7 @@ describe("normalizeURL", () => {
 
 		expect(actual).toEqual(expected);
 	});
+	// });
 
 	it("strips trailing `/` from the url input", () => {
 		const urlString = "https://example.com/path/";
@@ -32,5 +35,117 @@ describe("normalizeURL", () => {
 		const expected = "example.com/path";
 
 		expect(actual).toEqual(expected);
+	});
+});
+
+describe("getURLsFromHTML", () => {
+	// when no urls are found
+	it("returns an empty array when no urls are found", () => {
+		const htmlBody = `
+      <html>
+        <body>
+          <p>Some text</p>
+        </body>
+      </html>
+    `;
+		const baseURL = "https://example.com";
+		const actual = getURLsFromHTML(htmlBody, baseURL);
+		const expected = [];
+
+		expect(actual).toEqual(expected);
+	});
+
+	// when one url is found
+	it("returns an array of urls found", () => {
+		const htmlBody = `
+      <html>
+        <body>
+          <a href="https://another.example.com">Another Example</a>
+        </body>
+      </html>
+    `;
+
+		const baseURL = "https://example.com";
+		const actual = getURLsFromHTML(htmlBody, baseURL);
+		const expected = ["https://another.example.com/"];
+
+		expect(actual).toEqual(expected);
+	});
+
+	// when multiple urls are found
+	it("returns an array of urls found", () => {
+		const htmlBody = `
+      <html>
+        <body>
+          <a href="https://another.example.com">Another Example</a>
+          <a href="https://yet-another.example.com">Yet Another Example</a>
+        </body>
+      </html>
+    `;
+		const baseURL = "https://example.com";
+		const actual = getURLsFromHTML(htmlBody, baseURL);
+		const expected = [
+			"https://another.example.com/",
+			"https://yet-another.example.com/",
+		];
+
+		expect(actual).toEqual(expected);
+	});
+
+	// when relative urls are found
+	it("returns an array of urls found", () => {
+		const htmlBody = `
+      <html>
+        <body>
+          <a href="/another">Another Example</a>
+          <a href="/yet-another">Yet Another Example</a>
+        </body>
+      </html>
+    `;
+		const baseURL = "https://example.com";
+		const actual = getURLsFromHTML(htmlBody, baseURL);
+		const expected = [
+			"https://example.com/another",
+			"https://example.com/yet-another",
+		];
+
+		expect(actual).toEqual(expected);
+	});
+
+	// when page has both relative and absolute urls
+	it("returns an array of urls found", () => {
+		const htmlBody = `
+      <html>
+        <body>
+          <a href="/another">Another Example</a>
+          <a href="https://yet-another.example.com">Yet Another Example</a>
+        </body>
+      </html>
+    `;
+		const baseURL = "https://example.com";
+		const actual = getURLsFromHTML(htmlBody, baseURL);
+		const expected = [
+			"https://example.com/another",
+			"https://yet-another.example.com/",
+		];
+
+		expect(actual).toEqual(expected);
+	});
+
+	// when page has invalid url
+	it("returns an array of urls found", () => {
+		const htmlBody = `
+      <html>
+        <body>
+          <a href="invalid">Example</a>
+        </body>
+      </html>
+    `;
+		const baseURL = "https://example.com";
+		const actual = getURLsFromHTML(htmlBody, baseURL);
+		const expected = [];
+
+		expect(actual).toEqual(expected);
+		expect(actual).toThrow("Error: Invalid URL");
 	});
 });
