165253ee4f6cbec24a972b4bcf5b93a03157f1d3
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-11-16 00:20:32 +0000

    add crawlPage function + err handling
    no tests added :(

--- src/crawl.js
+++ src/crawl.js
@@ -1,5 +1,27 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+	try {
+		const response = await fetch(currentURL);
+
+		if (response.status > 399) {
+			console.log(`Error: ${response.status} on page: ${currentURL}`);
+			return;
+		}
+
+		const contentType = response.headers.get("content-type");
+
+		if (!contentType.includes("text/html")) {
+			console.log(`Error: ${contentType} on page: ${currentURL}`);
+			return;
+		}
+
+		console.log(await response.text());
+	} catch (error) {
+		console.log(`Error in fetch: ${error.message}, on page: ${currentURL}`);
+	}
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
 	const urls = [];
 	const dom = new JSDOM(htmlBody);
@@ -34,6 +56,7 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
+	crawlPage,
 	getURLsFromHTML,
 	normalizeURL,
 };
